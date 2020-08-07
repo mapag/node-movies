8ebce9bda77e3b2debf9fac55ec16e89752b6240
Mateo Pagniez <62345597+mapag@users.noreply.github.com>
2020-08-06 23:14:50 -0300

    [feat] ini api node

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+
+const app = express()
+
+const { config } = require('./config/index')
+
+app.get('/', function (req, res) {
+    res.send("hello world");
+})
+
+app.listen(config.port, function () {
+    console.log(`Listening http://localhost:${config.port}`)
+})
