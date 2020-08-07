a9c9fd7fdff26e8027536a56c38506d518b9a686
Mateo Pagniez <62345597+mapag@users.noreply.github.com>
2020-08-07 19:20:46 -0300

    boom implementation

--- index.js
+++ index.js
@@ -4,11 +4,22 @@ const app = express()
 const { config } = require('./config/index')
 const moviesApi = require('./routes/movies')
 
+const { logErrors, errorHandler, wrapError } = require('./utils/middleware/errorHandlers')
+const notFoundHandler = require('./utils/middleware/notFoundHandler')
 // parse application/json
 app.use(express.json())
 
 moviesApi(app)
 
+// Error middleware
+app.use(logErrors)
+app.use(wrapError)
+app.use(errorHandler)
+
+// Catch 404
+app.use(notFoundHandler)
+
 app.listen(config.port, function () {
+    // eslint-disable-next-line no-console
     console.log(`Listening http://localhost:${config.port}`)
 })
--- routes/movies.js
+++ routes/movies.js
@@ -12,7 +12,7 @@ function moviesApi(app) {
         const { tags } = req.query
         try {
             const movies = await moviesService.getDataByTags({ tags });
-
+            
             res.status(200).json({
                 data: movies,
                 message: 'movies listed'
