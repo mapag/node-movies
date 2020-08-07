ea94c6e0c767b956c07765f8530aa8e76aa5e1db
Mateo Pagniez <62345597+mapag@users.noreply.github.com>
2020-08-07 12:55:39 -0300

    remove body-parser

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 const express = require('express')
-const bodyParser = require('body-parser')
 const app = express()
 
 const { config } = require('./config/index')
 const moviesApi = require('./routes/movies')
 
 // parse application/json
-app.use(bodyParser.json())
+app.use(express.json())
 
 moviesApi(app)
 
